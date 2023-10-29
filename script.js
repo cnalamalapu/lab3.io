b0b26dcdca2846abfe93d886c117fe1064f0c732
cnalamalapu <143243923+cnalamalapu@users.noreply.github.com>
2023-10-29 02:04:50 -0500

    Update script.js
    Made JavaScript changes

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#mainForm').onchange = function() {
-        let billAmount = Number(document.getElementById('billTotal').value);
-        let tipPercent = document.getElementById('tipInput').value;
-        let tipPercentageDisplay = document.querySelector('.tip_percentage_input'); // Updated selector
-        let tipAmountDisplay = document.querySelector('#tipAmount');
-        let totalBillWithTipDisplay = document.querySelector('#totalBillWithTip');
-        tipPercentageDisplay.value = tipPercent;
-        let tipValue = billAmount * (tipPercent / 100);
-        let totalBillWithTip = billAmount + tipValue;
+    const mainForm = document.getElementById('mainForm');
+    const billTotalInput = document.getElementById('billTotal');
+    const tipInput = document.getElementById('tipInput');
+    const tipPercentageInput = document.querySelector('.tip_percentage_input');
+    const tipAmountDisplay = document.getElementById('tipAmount');
+    const totalBillWithTipDisplay = document.getElementById('totalBillWithTip');
+
+    mainForm.addEventListener('change', function() {
+        const billAmount = Number(billTotalInput.value);
+        const tipPercent = Number(tipInput.value);
+
+        tipPercentageInput.value = tipPercent;
+        const tipValue = (billAmount * tipPercent) / 100;
+        const totalBillWithTip = billAmount + tipValue;
+
         tipAmountDisplay.value = tipValue.toFixed(2);
         totalBillWithTipDisplay.value = totalBillWithTip.toFixed(2);
-    };
+    });
 });
