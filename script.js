a7371f84d785a03bf45cbf99ca055a279d1e448d
cnalamalapu <143243923+cnalamalapu@users.noreply.github.com>
2023-10-29 22:51:15 -0500

    Added validation script

--- script.js
+++ script.js
@@ -5,16 +5,43 @@ document.addEventListener('DOMContentLoaded', function() {
     const tipPercentageInput = document.querySelector('.tip_percentage_input');
     const tipAmountDisplay = document.getElementById('tipAmount');
     const totalBillWithTipDisplay = document.getElementById('totalBillWithTip');
+    const errorText = document.getElementById("errorText");
+
 
     mainForm.addEventListener('change', function() {
         const billAmount = Number(billTotalInput.value);
         const tipPercent = Number(tipInput.value);
-
+        const numberPattern = /^[0-9]+$/;
+        const inputValue = billTotalInput.value;
         tipPercentageInput.value = tipPercent;
         const tipValue = (billAmount * tipPercent) / 100;
         const totalBillWithTip = billAmount + tipValue;
-
-        tipAmountDisplay.value = tipValue.toFixed(2);
+        if (numberPattern.test(inputValue) || inputValue == "") {
+            tipAmountDisplay.value = tipValue.toFixed(2);
         totalBillWithTipDisplay.value = totalBillWithTip.toFixed(2);
+        }else {
+            totalBillWithTipDisplay.value= "";
+            tipAmountDisplay.value="";
+        }
+    });
+
+    billTotalInput.addEventListener("input", function () {
+        const numberPattern = /^[0-9]+$/;
+        const inputValue = billTotalInput.value;
+        if (numberPattern.test(inputValue) || inputValue == "") {
+            errorText.textContent = "";
+            errorText.style.display = "none";
+        } else {
+            errorText.textContent = "Please enter a valid number.";
+            errorText.style.color = "red";
+        errorText.style.fontSize = "14px";
+        errorText.style.marginTop = "5px";
+        errorText.style.display = "block";
+        totalBillWithTipDisplay.value= "";
+        tipAmountDisplay.value="";
+        }
     });
 });
+
+   
+
